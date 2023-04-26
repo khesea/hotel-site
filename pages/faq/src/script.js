a98a3d16fceb01e24b599b19db5946eaeca2528d
Ruslan <115349782+khesea@users.noreply.github.com>
2023-04-26 09:06:10 +0500

    faq

--- pages/faq/src/script.js
+++ pages/faq/src/script.js
@@ -2,3 +2,20 @@ function myFunction(x) {
   x.classList.toggle("change");
   document.querySelector('.container1__menu').classList.toggle('showMenu');
 }
+
+const theParent = document.querySelector('#theDude');
+theParent.addEventListener('click', doSomething);
+
+function doSomething(e) {
+  if (e.target != e.currentTarget) {
+    let clickedItem = e.target;
+    clickedItem.querySelector('.container2__dropContent').style.display = 'block';
+    clickedItem.querySelector('.moreinfo').style.display = 'none';
+    clickedItem.style.display = 'block';
+    clickedItem.style.border = 'none';
+    clickedItem.style.padding = '21px 24px';
+    clickedItem.style.background = '#E9ECE0';
+    clickedItem.classList.add('animationForText');
+  }
+  e.stopPropagation();
+}

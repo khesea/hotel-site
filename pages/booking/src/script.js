968669c5efb5183acfe64455fe7b92721d8d9d78
Ruslan <115349782+khesea@users.noreply.github.com>
2023-05-01 07:11:08 +0500

    index.html

--- pages/booking/src/script.js
+++ pages/booking/src/script.js
@@ -2,3 +2,17 @@ function myFunction(x) {
   x.classList.toggle("change");
   document.querySelector('.container1__menu').classList.toggle('showMenu');
 }
+let count = document.getElementById('count');
+let digit = 1;
+
+function minus() {
+  if (digit > 1) {
+    digit--;
+    count.textContent = digit;
+  }
+}
+
+function plus() {
+  digit++;
+  count.textContent = digit;
+}
